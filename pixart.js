ea54c67734e6978ec7674c7c2ad134dc7fb4faaf
Mabe-Diaby <diabymabe@gmail.com>
2021-10-29 22:14:01 -0400

    Prompt 1

--- pixart.js
+++ pixart.js
@@ -1,1 +1,18 @@
+/*
+Commit 1
+When I click the "Set Color" button, it should change the color of the "brush" box to the color I specify in the input field.
+You can use document.querySelector (or another document method) to select the element, then add an event listener.
+HINT: You will notice that the page refreshes whenever you click the button. You need to prevent this from happening using a method you have not used before. Google "javascript event prevent default".
+*/
+let setColorBtn = document.querySelector("#set-color")
+let colorField = document.querySelector("#color-field")
+let brushColor = document.querySelector(".brush")
 
+function noRefresh(event) {
+    event.preventDefault();
+    brushColor.style.backgroundColor = colorField.value
+}
+
+// always invoke preventDefault in the first line inside the function
+
+setColorBtn.addEventListener("click", noRefresh)
